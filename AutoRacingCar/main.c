04847febe8cdcc694e4dae615ff7b1a75d6c493c
Mads Skov <silentgamer2210@gmail.com>
2018-12-05 11:19:32 +0100

    Initial commit

---
+++ AutoRacingCar/main.c
@@ -0,0 +1,18 @@
+/*
+ * AutoRacingCar.c
+ *
+ * Created: 05/12/2018 10.58.53
+ * Author : SPRO Team 3
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
